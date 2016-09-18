01d1589dbbb3a182c5059a78d0c2e07ec0438537
Vadim Dyachkov <vadik.dyachkov@gmail.com>
2016-09-18 14:01:02 +0300

    add README.md and edit project structure

---
+++ core/src/main/java/ru/vaddya/schedule/core/Schedule.java
@@ -0,0 +1,10 @@
+package ru.vaddya.schedule.core;
+
+/**
+ * Created by Vadim on 9/18/2016.
+ */
+public class Schedule {
+    public static void main(String[] args) {
+        System.out.println("Hello schedule!");
+    }
+}
