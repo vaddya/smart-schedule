13375b7b815341296364ae6487e1308859cb5741
Vadim Dyachkov <vadik.dyachkov@gmail.com>
2016-09-22 21:44:09 +0300

    add json files

--- cli/src/main/java/ru/vaddya/schedule/cli/Main.java
+++ cli/src/main/java/ru/vaddya/schedule/cli/Main.java
@@ -2,12 +2,17 @@ package ru.vaddya.schedule.cli;
 
 import ru.vaddya.schedule.core.Schedule;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 /**
  * Created by Vadim on 9/22/2016.
  */
 public class Main {
     public static void main(String[] args) {
         Schedule schedule = new Schedule();
-        System.out.println("Today is " + schedule.getToday());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, d.M.Y", new Locale("ru"));
+        System.out.println("Сегодня " + dateFormat.format(schedule.getToday()));
+        System.out.println(schedule.test);
     }
 }
--- core/src/main/java/ru/vaddya/schedule/core/Schedule.java
+++ core/src/main/java/ru/vaddya/schedule/core/Schedule.java
@@ -1,6 +1,9 @@
 package ru.vaddya.schedule.core;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Scanner;
 
 /**
  * Created by Vadim on 9/18/2016.
@@ -9,8 +12,20 @@ public class Schedule {
 
     private Date today;
 
+    public String test;
+
     public Schedule() {
         this.today = new Date();
+
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            Scanner scan = new Scanner(
+                    new File(classLoader.getResource("schedule.json").getFile()));
+            scan.useDelimiter("\\Z");
+            test = scan.next();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public Date getToday() {
