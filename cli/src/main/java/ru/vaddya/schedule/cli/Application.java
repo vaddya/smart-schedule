d50df8cd4e6426419add5867a5b2e505f56cfaac
Vadim Dyachkov <vadik.dyachkov@gmail.com>
2016-10-24 22:55:24 +0300

    add app

---
+++ cli/src/main/java/ru/vaddya/schedule/cli/Application.java
@@ -0,0 +1,157 @@
+package ru.vaddya.schedule.cli;
+
+import ru.vaddya.schedule.core.Schedule;
+import ru.vaddya.schedule.core.ScheduleAPI;
+import ru.vaddya.schedule.core.lessons.Lesson;
+import ru.vaddya.schedule.core.tasks.Task;
+import ru.vaddya.schedule.core.utils.DaysOfWeek;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ * Класс для консольного взаимодействия с пользователем
+ *
+ * @author vaddya
+ */
+public class Application {
+
+    private ScheduleAPI schedule = new Schedule();
+    private Scanner in = new Scanner(System.in, "UTF-8");
+    private PrintStream out = System.out;
+
+    private static final String CANCEL = "q";
+
+    public void parseInput() {
+        String request;
+        out.print(">> ");
+        while (!CANCEL.equals(request = in.next())) {
+            switch (request) {
+                case "schedule":
+                    printSchedule();
+                    break;
+                case "tasks":
+                    printTasks();
+                    break;
+                case "active":
+                    printActive();
+                    break;
+                case "completed":
+                    printCompleted();
+                    break;
+                case "overdue":
+                    printOverdue();
+                    break;
+                case "add":
+                    parseAdd();
+                    break;
+                case "remove":
+                    parseRemove();
+                    break;
+                default:
+                    printHelp();
+                    break;
+            }
+            out.print(">> ");
+        }
+    }
+
+    public void printSchedule() {
+        out.println("\nРасписание");
+        out.println(schedule);
+    }
+
+    public void printTasks() {
+        out.println("\nЗадания:");
+        schedule.getAllTasks().forEach(System.out::println);
+    }
+
+    private void printActive() {
+        out.println("\nАктивные задания:");
+        schedule.getActiveTasks().forEach(System.out::println);
+    }
+
+    private void printCompleted() {
+        out.println("\nВыполненные задания:");
+        schedule.getCompletedTasks().forEach(System.out::println);
+    }
+
+    private void printOverdue() {
+        out.println("\nПросроченные задания:");
+        schedule.getOverdueTasks().forEach(System.out::println);
+    }
+
+    private void printHelp() {
+        out.println("Usage of Smart Schedule: ");
+        out.println("\t>> schedule - print schedule");
+        out.println("\t>> tasks - print all tasks");
+        out.println("\t>> active - print active tasks");
+        out.println("\t>> completed - print completed tasks");
+        out.println("\t>> overdue - print overdue tasks");
+        out.println("\t>> add lesson - add lesson to the lesson list");
+        out.println("\t>> add task - add task to the task list");
+        out.println("\t>> remove lesson - remove lesson from the lesson list");
+        out.println("\t>> remove task - remove task from the task list");
+        out.println("\t>> help - print help");
+    }
+
+    private void parseAdd() {
+        String kind = in.next();
+        if ("lesson".equals(kind)) {
+            out.print("Day of week: ");
+            DaysOfWeek day = DaysOfWeek.valueOf(in.next().toUpperCase());
+            schedule.addLesson(day, parseLesson());
+        } else if ("task".equals(kind)) {
+            schedule.addTask(parseTask());
+        } else {
+            printHelp();
+        }
+    }
+
+    private void parseRemove() {
+        String kind = in.next();
+        if ("lesson".equals(kind)) {
+            out.print("Day of week: ");
+            DaysOfWeek day = DaysOfWeek.valueOf(in.next().toUpperCase());
+            out.print("Lesson number: ");
+            int i = in.nextInt();
+            schedule.removeLesson(day, i);
+        } else if ("task".equals(kind)) {
+            out.print("Task number: ");
+            int i = in.nextInt();
+            schedule.removeTask(i);
+        } else {
+            printHelp();
+        }
+    }
+
+    private Task parseTask() {
+        Task.Builder builder = new Task.Builder();
+        out.print("Subject: ");
+        builder.subject(in.next());
+        out.print("Lesson type: ");
+        builder.type(in.next());
+        out.print("Deadline: ");
+        builder.deadline(in.next());
+        out.print("Task: ");
+        builder.textTask(in.next());
+        return builder.build();
+    }
+
+    private Lesson parseLesson() {
+        Lesson.Builder builder = new Lesson.Builder();
+        out.print("Start time: ");
+        builder.startTime(in.next());
+        out.print("End time: ");
+        builder.endTime(in.next());
+        out.print("Subject: ");
+        builder.subject(in.next());
+        out.print("Lesson type: ");
+        builder.type(in.next());
+        out.print("Place: ");
+        builder.place(in.next());
+        out.print("Teacher: ");
+        builder.teacher(in.next());
+        return builder.build();
+    }
+}
