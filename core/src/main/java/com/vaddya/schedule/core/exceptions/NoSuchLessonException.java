128465a54d182a616e6effef1d1581ed28df5eaf
vaddya <vadik.dyachkov@gmail.com>
2017-03-20 14:48:13 +0300

    add files

---
+++ core/src/main/java/com/vaddya/schedule/core/exceptions/NoSuchLessonException.java
@@ -0,0 +1,14 @@
+package com.vaddya.schedule.core.exceptions;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Бросается при отсутствии запрашиваемого занятия
+ *
+ * @author vaddya
+ */
+public class NoSuchLessonException extends NoSuchElementException {
+    public NoSuchLessonException(String s) {
+        super(s);
+    }
+}
